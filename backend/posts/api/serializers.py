6304cc5b2d3877f00caa1cd3ebf586e0e028d8b7
JodelDjan <w1973296@westminster.ac.uk>
2026-02-09 14:58:56 +0000

    created application model and serialiser

--- backend/posts/api/serializers.py
+++ backend/posts/api/serializers.py
@@ -1,9 +1,23 @@
 #from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
-from ..models import Post
+from ..models import Post, Application
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'body']
         
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = [
+            'id',
+            'post',
+            'user',
+            'has_read_post',
+            'has_consented',
+            'created_at'
+        ]
+        read_only_fields = ['user', 'post']
+
--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -6,3 +7,27 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Post: {self.title}"
+    
+class Application(models.Model):
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        related_name='applications'
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    # New fields
+    has_read_post = models.BooleanField(default=False)
+    has_consented = models.BooleanField(default=False)
+
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')
+
+    def __str__(self):
+        return f"{self.user.username} applied to {self.post.title}"
