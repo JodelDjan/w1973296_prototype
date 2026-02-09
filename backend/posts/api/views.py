1eb1404b027e6c7df88af3fb5c225396f012812c
JodelDjan <w1973296@westminster.ac.uk>
2026-02-09 15:31:45 +0000

    created permissions.py

--- backend/posts/api/views.py
+++ backend/posts/api/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, permissions
 from ..models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, ApplicationSerializer
+from ..permissions import IsGeneralUser
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+class ApplyToPostView(generics.CreateAPIView):
+    serializer_class = ApplicationSerializer
+    permission_classes = [permissions.IsAuthenticated, IsGeneralUser]
+
+    def perform_create(self, serializer):
+        post = Post.objects.get(id=self.kwargs['post_id'])
+        serializer.save(
+            user=self.request.user,
+            post=post
+        )
