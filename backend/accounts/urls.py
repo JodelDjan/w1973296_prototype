c63102cff97f19b43261f670a39120c08cee981d
JodelDjan <w1973296@westminster.ac.uk>
2026-02-10 13:18:42 +0000

    created me.view in accounts urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import RegisterView
 from django.urls import path
-from .views import RegisterView, EmailLoginView
+from .views import RegisterView, EmailLoginView, MeView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', EmailLoginView.as_view(), name='email_login'),
+    path('me/', MeView.as_view(), name='me'),
 ]
 
