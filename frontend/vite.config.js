258cda939c3ada35a80cc7b10c3c2003dedba3f8
JodelDjan <w1973296@westminster.ac.uk>
2026-02-11 22:05:53 +0000

    Fix _redirects for SPA routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
   },
-  publicDir: 'public'
+  publicDir: 'public', 
 })
